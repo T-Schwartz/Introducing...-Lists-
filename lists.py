334714dd186d58136b534370d9946a412ca18a63
T-Schwartz <56375130+T-Schwartz@users.noreply.github.com>
2020-03-11 19:43:05 -0400

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+team=["Seahwks", 2014, "CentryLink Field"]
+nums=[5,10,4,5]
+words=["Spam", "ni"]
+
+print (team)
+
+#list functions
+print ("Length words: ", len(words))
+print ("Max nums: ", max(nums))
+print ("Min nums", min(nums))
+print (nums.count(5)) # how many number 5's their are in the list
+print (nums.index(4)) #what index is the number 4 in
+
+revTeam=team.reverse()
+print ("team: ", team)
+print ("revTeam: ", revTeam)
