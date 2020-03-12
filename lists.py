36f504a8e512bef56be4949fd49894c80d85ddbc
T-Schwartz <56375130+T-Schwartz@users.noreply.github.com>
2020-03-11 21:23:37 -0400

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,16 +1,16 @@
-team=["Seahwks", 2014, "CentryLink Field"]
-nums=[5,10,4,5]
-words=["Spam", "ni"]
+book=["Harry Potter", 1997, "JK Rowling"]
+nums=[7,16,3,7]
+words=["Double", "blasters"]
 
-print (team)
+print (book)
 
 #list functions
 print ("Length words: ", len(words))
 print ("Max nums: ", max(nums))
 print ("Min nums", min(nums))
 print (nums.count(5)) # how many number 5's their are in the list
-print (nums.index(4)) #what index is the number 4 in
+print (nums.index(3)) #what index is the number 4 in
 
-revTeam=team.reverse()
-print ("team: ", team)
-print ("revTeam: ", revTeam)
+book.reverse()
+
+print ("reverse book: ", book)
